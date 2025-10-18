dfd0803d0c9a556bb957dbe3c919aa587afb880c
leeyeonwoo <rexrex1105@gmail.com>
2025-10-18 16:08:04 +0900

    feat(pkg): add config package, add example config yml

---
+++ pkg/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type AppConfig struct {
+	Checker struct {
+		CheckPods        bool `yaml:"check_pods"`
+		CheckNodes       bool `yaml:"check_nodes"`
+		CheckDeployments bool `yaml:"check_deployments"`
+	} `yaml:"checker"`
+	
+	Slack struct {
+		WebhookURL string `yaml:"webhook_url"`
+	} `yaml:"slack"`
+}
+
+func LoadConfig(path string) (*AppConfig, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	
+	var config AppConfig
+	err = yaml.Unmarshal(data, &config)
+	return &config, err
+}
